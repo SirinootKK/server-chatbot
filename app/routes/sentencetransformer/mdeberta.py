0685689e8b761b02ab05b79e4ed20ded4d5f526f
SirinootKK <sirinoot981@gmail.com>
2024-02-21 23:02:06 +0700

    change param

---
+++ app/routes/sentencetransformer/mdeberta.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, request,jsonify
+from app.transformersmodel import TransformersModel
+
+bertmdeberta_blueprint = Blueprint('bertmde',__name__)
+
+model_path = 'app/model/medeberta'
+# tokenizer_path = 'app/model/medeberta'
+embeddings_path = 'app/model/embeddings.pkl'
+sentenceEmbeddingModel='intfloat/multilingual-e5-base'
+dataset_path = 'app/dataset.xlsx'
+
+bertmde = TransformersModel(df_path=dataset_path, model_path=model_path, tokenizer_path=model_path, embedding_model_name=sentenceEmbeddingModel, embeddingsPath=embeddings_path)
+
+@bertmdeberta_blueprint.route('/api/get_semantic_mde', methods=['POST'])
+def get_semantic_mde():
+    data = request.get_json()
+    user_message = data['message']
+
+    answer, context,score,distance,start_index, end_index = bertmde.predict_bert_embedding(user_message)
+
+    print('semantic mdeberta')
+
+    return jsonify({'semantic_wc': answer, 'score_wc':score ,'context_semantic_wc':context, 'info_distance_wc':distance, "start_index":start_index, "end_index":end_index})
+
+
---
+++ app/routes/sentencetransformer/wangchanberta.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, request,jsonify
+from app.transformersmodel import TransformersModel
+
+bertwangchanberta_blueprint = Blueprint('bertwc',__name__)
+
+model_path = 'app/model/wangchanberta'
+embeddings_path = 'app/model/embeddings.pkl'
+sentenceEmbeddingModel='intfloat/multilingual-e5-base'
+dataset_path = 'app/dataset.xlsx'
+
+bertwc = TransformersModel(df_path=dataset_path, model_path=model_path, tokenizer_path=model_path, embedding_model_name=sentenceEmbeddingModel, embeddingsPath=embeddings_path)
+
+@bertwangchanberta_blueprint.route('/api/get_semantic_wc', methods=['POST'])
+def get_semantic_wc():
+    data = request.get_json()
+    user_message = data['message']
+
+    answer, context,score,distance,start_index, end_index = bertwc.predict_bert_embedding(user_message)
+
+
+    return jsonify({'semantic_wc': answer, 'score_wc':score ,'context_semantic_wc':context, 'info_distance_wc':distance, "start_index":start_index, "end_index":end_index})
+
+
