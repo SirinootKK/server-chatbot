b422af784ed9e2733cc245da25ffbb9a82bd70ac
SirinootKK <sirinoot981@gmail.com>
2024-02-21 23:23:09 +0700

    fix file name

--- app/routes/semantic_search/mdeberta.py
+++ app/routes/semantic_search/mdeberta.py
@@ -9,17 +9,17 @@ embeddings_path = 'app/model/embeddings.pkl'
 sentenceEmbeddingModel='intfloat/multilingual-e5-base'
 dataset_path = 'app/dataset.xlsx'
 
-semanticmde = TransformersModel(df_path=dataset_path, model_path=model_path, tokenizer_path=model_path, embedding_model_name=sentenceEmbeddingModel, embeddingsPath=embeddings_path, chunk_size=1000)
+semanticmde = TransformersModel(df_path=dataset_path, model_path=model_path, tokenizer_path=model_path, embedding_model_name=sentenceEmbeddingModel, embeddingsPath=embeddings_path)
 
 @semanticmdeberta_blueprint.route('/api/get_semantic_mde', methods=['POST'])
 def get_semantic_mde():
     data = request.get_json()
     user_message = data['message']
 
-    answer, context, score,distance = semanticmde.predict_bert_embedding(user_message)
+    answer, context, score,distance,start_index,end_index= semanticmde.predict_bert_embedding(user_message)
 
     print('semantic mdeberta')
 
-    return jsonify({'semantic_mde': answer, 'score':score ,'context_semantic_mde':context, 'info_distance':distance})
+    return jsonify({'semantic_mde': answer, 'score':score ,'context_semantic_mde':context, 'info_distance':distance, 'start_index':start_index, 'end_index':end_index})
 
 
