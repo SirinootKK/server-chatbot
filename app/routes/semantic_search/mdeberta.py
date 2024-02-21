d713ae01dfb2b000eccabe35c03ea923b17477b9
SirinootKK <sirinoot981@gmail.com>
2024-02-21 23:16:03 +0700

    Revert "fix argument"
    This reverts commit a2be5d27d321b8d37fc732502b5820ea0a239936.

---
+++ app/routes/semantic_search/mdeberta.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, request,jsonify
+from app.transformersmodel import TransformersModel
+
+semanticmdeberta_blueprint = Blueprint('semanticmde',__name__)
+
+model_path = 'app/model/medeberta'
+# tokenizer_path = 'app/model/medeberta'
+embeddings_path = 'app/model/embeddings.pkl'
+sentenceEmbeddingModel='intfloat/multilingual-e5-base'
+dataset_path = 'app/dataset.xlsx'
+
+semanticmde = TransformersModel(df_path=dataset_path, model_path=model_path, tokenizer_path=model_path, embedding_model_name=sentenceEmbeddingModel, embeddingsPath=embeddings_path, chunk_size=1000)
+
+@semanticmdeberta_blueprint.route('/api/get_semantic_mde', methods=['POST'])
+def get_semantic_mde():
+    data = request.get_json()
+    user_message = data['message']
+
+    answer, context, score,distance = semanticmde.predict_bert_embedding(user_message)
+
+    print('semantic mdeberta')
+
+    return jsonify({'semantic_mde': answer, 'score':score ,'context_semantic_mde':context, 'info_distance':distance})
+
+
